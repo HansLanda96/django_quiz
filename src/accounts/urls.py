0a14d239c293cec9e8ab2f858d652779dbc427b1
Iegor <duuurrr17@gmail.com>
2022-11-11 07:02:47 +0200

    Modify accounts/urls .
    Add path for user_reactivate function that will resend activation link for registred user.

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,22 +1,35 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from .views import UserLoginView
-from .views import UserLogoutView
-from .views import UserProfileUpdateView
-from .views import UserRegisterView
-from .views import user_activate
-from .views import user_profile_view
+from .views import (
+    UserLoginView, UserLogoutView,
+    UserProfileUpdateView, UserRegisterView,
+    user_activate, user_profile_view, user_reactivate,
+)
+
 
 app_name = 'accounts'
 
 urlpatterns = [
+    # register urls
     path('register/activate/<str:sign>/', user_activate, name='register_activate'),
-    path('register/done/',
-         TemplateView.as_view(template_name='accounts/user_register_done.html'), name='register_done'),
+
+    path(
+        'register/done/',
+        TemplateView.as_view(template_name='accounts/user_register_done.html'),
+        name='register_done'
+    ),
+
     path('register/', UserRegisterView.as_view(), name='register'),
+
+    # login / logout urls
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
+
+    # profile urls
     path('profile/', user_profile_view, name='profile'),
     path('profile/update/', UserProfileUpdateView.as_view(), name='profile_update'),
+
+    # resend activation email
+    path('activation/failed/', user_reactivate, name='activation_failed'),
 ]
